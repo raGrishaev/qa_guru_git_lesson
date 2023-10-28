2b3fd367050adba0f4335c5a97ecdf772fd6b00d
raGrishaev <97452288+raGrishaev@users.noreply.github.com>
2023-10-29 02:06:05 +0300

    Update Main.java

--- src/main/java/ru/grishaev/Main.java
+++ src/main/java/ru/grishaev/Main.java
@@ -2,6 +2,11 @@ package ru.grishaev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage(){
+        String msg = "Hello, qa.guru!!!!!";
+        return msg;
     }
 }
