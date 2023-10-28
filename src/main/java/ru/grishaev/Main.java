aba1e2f51f70db9ca157595eccde37edb3baa2df
Roman Grishaev <grishaev.r-a@ya.ru>
2023-10-29 02:07:02 +0300

    - added new dependecy(javaFaker) - added getMessage method

--- src/main/java/ru/grishaev/Main.java
+++ src/main/java/ru/grishaev/Main.java
@@ -1,7 +1,14 @@
 package ru.grishaev;
 
+import com.github.javafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage(){
+        var faker = new Faker();
+        return "Hello, " + faker.name().firstName() + "!";
     }
 }
